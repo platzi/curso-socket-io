4db4012bf95575eacf8d4092cc2e95e6231abdbe
RetaxMaster <retaxmaster@hotmail.com>
2022-05-16 12:38:57 -0500

    10 On, Once, Off

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -7,13 +7,39 @@ socket.on("welcome", data => {
 
 const emitToServer = document.querySelector("#emit-to-server");
 emitToServer.addEventListener("click", () => {
-
     socket.emit("server", "Hola, servidor 👀");
-
 });
 
 socket.on("everyone", message => {
-
     console.log(message);
+});
+
+const emitToLast = document.querySelector("#emit-to-last");
+emitToLast.addEventListener("click", () => {
+
+    socket.emit("last", "Hola 😄");
 
 });
+
+socket.on("salute", message => {
+    console.log(message);
+});
+
+// on, once, off
+socket.on("on", () => {
+    console.log("Se emite varias veces");
+});
+
+socket.once("once", () => {
+    console.log("Se emite una sola vez");
+});
+
+const listener = () => {
+    console.log("Se apaga el evento");
+}
+
+socket.on("off", listener);
+
+setTimeout(() => {
+    socket.off("off", listener);
+}, 2000);
