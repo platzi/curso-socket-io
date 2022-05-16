13644d1c6a9f36707d296cf3291b3780d6b8e992
RetaxMaster <retaxmaster@hotmail.com>
2022-05-16 11:12:29 -0500

    Clase 7 Primer servidor

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use( express.static(path.join(__dirname, "views")) );
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+io.on("connection", socket => {
+
+    console.log(socket.id);
+
+});
+
+httpServer.listen(3000);
