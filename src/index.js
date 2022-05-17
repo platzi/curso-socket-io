d2dca3bafc90c7bc14c729f24e4372b57c4953b0
RetaxMaster <retaxmaster@hotmail.com>
2022-05-17 11:46:40 -0500

    15 Offline

--- src/index.js
+++ src/index.js
@@ -13,32 +13,11 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-/* io.on("connection", socket => {
+io.on("connection", socket => {
 
-    io.emit();
-
-}); */
-
-const teachers = io.of("teachers");
-const students = io.of("students");
-
-teachers.on("connection", socket => {
-
-    console.log(socket.id + " se ha conectado a la sala de profes");
-
-    socket.on("send message", data => {
-        teachers.emit("message", data);
-    });
-
-});
-
-students.on("connection", socket => {
-
-    console.log(socket.id + " se ha conectado a la sala de estudiantes");
-
-    socket.on("send message", data => {
-        students.emit("message", data);
-    });
+    socket.on("is connected", msg => {
+        console.log(msg);
+    })
 
 });
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,46 +1,20 @@
-// const socket = io();
+const socket = io();
 
-const user = prompt("Escribe tu usuario");
+const send = document.querySelector("#send");
+const disconnect = document.querySelector("#disconnect");
+const reconnect = document.querySelector("#reconnect");
 
-const profes = ["RetaxMaster", "juandc", "GNDX"];
+send.addEventListener("click", () => {
 
-let socketNamespace, group;
-
-const chat = document.querySelector("#chat");
-const namespace = document.querySelector("#namespace");
-
-if (profes.includes(user)) {
-    socketNamespace = io("/teachers");
-    group = "teachers";
-}
-else {
-    socketNamespace = io("/students");
-    group = "students";
-}
-
-socketNamespace.on("connect", () => {
-    namespace.textContent = group;
-});
-
-// Programando la lógica de envío de mensajes
-
-const sendMessage = document.querySelector("#sendMessage");
-sendMessage.addEventListener("click", () => {
-
-    const message = prompt("Escribe tu mensaje: ");
-
-    socketNamespace.emit("send message", {
-        message, user
-    });
+    if(socket.connected)
+        socket.emit("is connected", "¡Está conectado!");
 
 });
 
-socketNamespace.on("message", messageData => {
+disconnect.addEventListener("click", () => {
+    socket.disconnect();
+});
 
-    const { user, message } = messageData;
-
-    const li = document.createElement("li");
-    li.textContent = `${user}: ${message}`;
-
-    chat.append(li);
+reconnect.addEventListener("click", () => {
+    socket.connect();
 });
